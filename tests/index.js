296e68c196ba0be8b47b9d87d3a3efce2987933b
blaing <billy.laing@turner.com>
2016-09-25 18:27:07 -0400

    Update tests

--- tests/index.js
+++ tests/index.js
@@ -9,16 +9,18 @@ test('environment variable not set', (assert) => {
 
     delete process.env[VARIABLE_NAME];
 
-    const testVar = env.get(VARIABLE_NAME, DEFAULT_VALUE);
+    const withDefault = env.get(VARIABLE_NAME, DEFAULT_VALUE);
 
     assert.equals(
-        testVar,
+        withDefault,
         DEFAULT_VALUE,
         'If ENV variable is undefined and default set should return default.'
     );
 
+    const noDefault = () => env.get(VARIABLE_NAME);
+
     assert.throws(
-        () => env.get(VARIABLE_NAME),
+        noDefault,
         'If ENV variable is undefined and no default should throw an exception.'
     );
 
@@ -30,16 +32,18 @@ test('empty string environment variable', (assert) => {
 
     process.env[VARIABLE_NAME] = "";
 
-    const testVar = env.get(VARIABLE_NAME, DEFAULT_VALUE);
+    const withDefault = env.get(VARIABLE_NAME, DEFAULT_VALUE);
 
     assert.equals(
-        testVar,
+        withDefault,
         DEFAULT_VALUE,
         'If there is an empty string in ENV it should use the default.'
     );
 
+    const noDefault = () => env.get(VARIABLE_NAME);
+
     assert.throws(
-        () => env.get(VARIABLE_NAME),
+        noDefault,
         'If there is an empty string in ENV and no default should throw an error.'
     );
 
