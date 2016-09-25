66c3ad707a56b6fbbeff939797ed95254fa6fa76
blaing <billy.laing@turner.com>
2016-09-25 11:28:22 -0400

    Add tests

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ module.exports = {
     get: function get(variableName, optionalDefault) {
         var value = process.env[variableName] || optionalDefault;
         if (typeof value != 'undefined') return value;
-        throw new Error('Env variable ' + variableName + 'not found, and no default supplied!');
+        throw new Error('Env variable ' + variableName + ' not found, and no default supplied!');
     }
 };
---
+++ tests/get.js
@@ -0,0 +1,64 @@
+var test = require('tape');
+var env = require('../');
+var TEST_VARIABLE = 'TEST_VARIABLE';
+
+test('no var and no default should throw an error', function(t) {
+    t.plan(1);
+
+    delete process.env[TEST_VARIABLE];
+
+    t.throws(
+        function() {
+            env.get(TEST_VARIABLE);
+        },
+        'If ENV variable is undefined and no default should throw an exception.'
+    );
+
+});
+
+test('no var and default should return the default', function(t) {
+    t.plan(1);
+
+    delete process.env[TEST_VARIABLE];
+    var defaultValue = 'default';
+
+    var testVar = env.get(TEST_VARIABLE, defaultValue);
+
+    t.equals(
+        testVar,
+        defaultValue,
+        'If there is an empty string in ENV it should use the default.'
+    );
+
+});
+
+test('empty string and no default should throw an error', function(t) {
+    t.plan(1);
+
+    process.env[TEST_VARIABLE] = "";
+
+    t.throws(
+        function() {
+            env.get(TEST_VARIABLE);
+        },
+        'Env variable ' + TEST_VARIABLE + ' not found, and no default supplied!',
+        'If ENV variable is undefined and no default should throw an error.'
+    )
+
+});
+
+test('empty string and default should return default', function(t) {
+    t.plan(1);
+
+    process.env.TEST = "";
+    var defaultValue = 'default';
+
+    var testVar = env.get(TEST_VARIABLE, defaultValue);
+
+    t.equals(
+        testVar,
+        defaultValue,
+        'If there is an empty string in ENV it should use the default.'
+    );
+
+});
